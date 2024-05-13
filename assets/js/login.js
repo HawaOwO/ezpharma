27a2d6175b936f45d5bd87d93991e052e8950c4a
HawaOwO <dayinialhawani@student.usm.my>
2024-05-13 18:31:07 +0800

    validation input done

--- assets/js/login.js
+++ assets/js/login.js
@@ -20,7 +20,7 @@ login.addEventListener('click', async (e) => {
         if (userData && userData.role === 'Admin') {
             // Only allow login if the user's role is 'Admin'
             alert('User logged in successfully!');
-            window.location.href = 'index.html';
+            window.location.href = 'home.html';
         } else {
             // If user's role is not 'Admin', sign them out and display an error message
             await signOut(auth);
@@ -32,33 +32,4 @@ login.addEventListener('click', async (e) => {
     }
 });
 
-        // login.addEventListener('click', (e) => {
-        //     var email = document.getElementById('email').value;
-        //     var password = document.getElementById('password').value;
-        
-        //     signInWithEmailAndPassword(auth, email, password)
-        //         .then(async (userCredential) => {
-        //             const user = userCredential.user;
-        
-        //             // Fetch the user's role from the database
-        //             const userDataRef = ref(database, `User/${user.uid}`);
-        //             const userDataSnapshot = await get(userDataRef);
-        //             const userData = userDataSnapshot.val();
-        
-        //             if (userData && userData.role === 'Admin') {
-        //                 // Only allow login if the user's role is 'Admin'
-        //                 alert('User logged in successfully!');
-        //                 window.location.href = 'index.html';
-        //             } else {
-        //                 // If user's role is not 'Admin', sign them out and display an error message
-        //                 await signOut(auth);
-        //                 alert('Only users with Admin role can log in.');
-        //             }
-        //         })
-        //         .catch((error) => {
-        //             const errorCode = error.code;
-        //             const errorMessage = error.message;
-        //             alert(errorMessage);
-        //         });
-        // });
-        
+  
